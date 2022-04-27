1b0448ce22a2ee8f7498da4a29150258f5f04178
永野晴彦 <naganoharuhiko@naganoharuhikonoMacBook-Air.local>
2022-04-27 23:45:52 +0900

    normalモデム追加

--- app/controllers/normals_controller.rb
+++ app/controllers/normals_controller.rb
@@ -1,10 +1,30 @@
 class NormalsController < ApplicationController
+  def index
+    @normals = Normal.all
+  end
+  
   def new
+    @normal = Normal.new
+  end
+
+  def create
+    @normal = current_user.normals.build(normal_params)
+    if @normal.save
+      redirect_to normals_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @normal = Normal.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def normal_params
+    params.require(:normal).permit(:before_address, :current_address, :trigger, :preparation) 
   end
 end
