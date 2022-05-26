37fb2b2081fde80d6143be1a327c2c7eb448fbe9
永野晴彦 <naganoharuhiko@naganoharuhikonoMacBook-Air.local>
2022-05-26 11:35:09 +0900

    room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_235816) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_023357) do
   create_table "blogs", force: :cascade do |t|
     t.integer "user_id"
     t.string "title_blog"
@@ -128,6 +128,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_235816) do
     t.integer "message_id"
     t.integer "work_id"
     t.integer "lifestyle_id"
+    t.integer "room_id"
     t.index ["comment_id"], name: "index_notifications_on_comment_id"
     t.index ["comment_lifestyle_id"], name: "index_notifications_on_comment_lifestyle_id"
     t.index ["comment_work_id"], name: "index_notifications_on_comment_work_id"
